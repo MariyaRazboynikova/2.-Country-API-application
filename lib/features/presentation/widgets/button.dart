943718ca57b40bec8204c2e95dced42ea294db40
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-12-12 10:23:12 +0500

    1

---
+++ lib/features/presentation/widgets/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ButtonFotInitialPage extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  const ButtonFotInitialPage({super.key, required this.text, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 300,
+        decoration: ,
+      ),
+    );
+  }
+}
