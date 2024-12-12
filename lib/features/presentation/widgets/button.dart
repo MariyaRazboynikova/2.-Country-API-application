f16496d5a3ac72de157df92c26de9b2197cc318a
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-12-12 10:56:01 +0500

    1

--- lib/features/presentation/widgets/button.dart
+++ lib/features/presentation/widgets/button.dart
@@ -9,7 +9,34 @@ class ButtonFotInitialPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(width: 300, decoration: BoxDecoration()),
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
